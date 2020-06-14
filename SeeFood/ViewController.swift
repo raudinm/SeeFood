4bbc472c23ad0cc82fac84190c356797ac22bc54
Raudin Moreno <raudin247@gmail.com>
2020-06-14 16:18:52 -0400

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Raudin Moreno on 6/14/20.
+//  Copyright © 2020 Raudin Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
